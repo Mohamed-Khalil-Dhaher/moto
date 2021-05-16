868e2fb607b9294d2a844e5e388ce4208bd014b5
Mohamed-Khalil-Dhaher <dhaherkhalil91@gmail.com>
2021-05-16 05:55:12 +0100

    search bar work

--- src/components/Motos.jsx
+++ src/components/Motos.jsx
@@ -24,7 +24,7 @@ class Motos extends React.Component {
     this.setState({
       moto: newMoto,
     });
-    console.log(this.state.moto);
+    //this.props.changeView("search");
   }
   changeMoto(event) {
     // console.log(this.state.moto);
@@ -55,9 +55,33 @@ class Motos extends React.Component {
         </div>
         <br></br>
         <div>
+          {/* <div>
+            <h4>type:{this.state.moto.type}</h4>
+            <h4>description:{this.state.moto.description}</h4>
+          </div> */}
           <div>
-            {/* <h4>type:{this.state.moto.type}</h4>
-            <h4>description:{this.state.moto.description}</h4> */}
+            <body>
+              <div class="wrapper">
+                <div class="product-img">
+                  <img src={this.state.moto.image} height="420" width="600" />
+                </div>
+                <div class="product-info">
+                  <div class="product-text">
+                    <h1>{this.state.moto.type}</h1>
+                    <h2>Model :{this.state.moto.model}</h2>
+                    <h2>Contact :{this.state.moto.contactInformation}</h2>
+                    <p>
+                      {this.state.moto.description}
+                      <br></br>
+                    </p>
+                  </div>
+                  <div class="product-price-btn">
+                    <span>{this.state.moto.price}</span>DT<br></br>
+                    <button type="button">buy now</button>
+                  </div>
+                </div>
+              </div>
+            </body>
           </div>
           {this.props.motos.map((moto) => {
             return <MotoDetails moto={moto} />;
